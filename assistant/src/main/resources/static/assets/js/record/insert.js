2e0a1a41fd347523b4ce92856aafa6943717db57
fanwanyi123 <534310620@qq.com>
2019-08-16 23:07:04 +0800

    加了附件

--- assistant/src/main/resources/static/assets/js/record/insert.js
+++ assistant/src/main/resources/static/assets/js/record/insert.js
@@ -1,6 +1,6 @@
 var vm;
 $(document).ready(function (e) {
-    vueFunction("#app",e);
+    vueFunction("#app", e);
     initCategory();
     // Forum Note Active
     $('#summernote').summernote({
@@ -11,8 +11,8 @@ $(document).ready(function (e) {
     });
 });
 
-function vueFunction(el,e) {
-     vm = new Vue({
+function vueFunction(el, e) {
+    vm = new Vue({
         el: el,
         data: {
             categoryList: [],
@@ -22,23 +22,24 @@ function vueFunction(el,e) {
             showThird: false,
             valueOne: [],
             valueTwo: [],
-            valueThree: []
+            valueThree: [],
+            fileList: []
         },
         e: e,
-        methods:{
+        methods: {
             getChildTag: function (val) {
-                if (val == 1){
-                    $("#tagIcon").attr("style","color:green;");
-                } else if (val == 2){
-                    $("#tagIcon").attr("style","color:yellow");
+                if (val == 1) {
+                    $("#tagIcon").attr("style", "color:green;");
+                } else if (val == 2) {
+                    $("#tagIcon").attr("style", "color:yellow");
                 } else {
-                    $("#tagIcon").attr("style","color:red");
+                    $("#tagIcon").attr("style", "color:red");
                 }
                 vm.showSecond = false;
                 vm.childTagList = [];
-                if (val){
-                    for (var i=0; i < vm.categoryList.length;i++){
-                        if (val == vm.categoryList[i].pid){
+                if (val) {
+                    for (var i = 0; i < vm.categoryList.length; i++) {
+                        if (val == vm.categoryList[i].pid) {
                             var set = new Set(vm.childTagList);
                             set.add(vm.categoryList[i]);
                             vm.childTagList = Array.from(set);
@@ -50,9 +51,9 @@ function vueFunction(el,e) {
             getGrandSonTag: function (val) {
                 vm.showThird = false;
                 vm.grandsonList = [];
-                if (val){
-                    for (var i=0; i < vm.categoryList.length;i++){
-                        if (val == vm.categoryList[i].pid){
+                if (val) {
+                    for (var i = 0; i < vm.categoryList.length; i++) {
+                        if (val == vm.categoryList[i].pid) {
                             var set = new Set(vm.grandsonList);
                             set.add(vm.categoryList[i]);
                             vm.grandsonList = Array.from(set)
@@ -61,12 +62,24 @@ function vueFunction(el,e) {
                     }
                 }
             },
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+            },
+            handleExceed(files, fileList) {
+                this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+            },
+            beforeRemove(file, fileList) {
+                return this.$confirm(`确定移除 ${ file.name }？`);
+            }
         }
     })
 }
 
 
-function initCategory(){
+function initCategory() {
     $.ajax({
         url: getRootPath() + "/record/getCategory",
         type: "post",
